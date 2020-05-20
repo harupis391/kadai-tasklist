becd7ffada7a2308921a42353626804ac54d0f73
harupis <miiraandlee39391@gmail.com>
2020-05-20 08:59:10 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
-  get 'sessions/new'
-  get 'sessions/create'
-  get 'sessions/destroy'
+  get 'users/new'
+  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   
